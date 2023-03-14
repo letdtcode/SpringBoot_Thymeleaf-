9972d3ff48cba2f1e81c5e6786332afdd77054c8
letdtcode <boynguyenducthanh@gmail.com>
2023-03-15 02:09:49 +0700

    update admin controller

--- src/main/java/com/sheryians/major/MajorApplication.java
+++ src/main/java/com/sheryians/major/MajorApplication.java
@@ -1,13 +1,12 @@
 package com.sheryians.major;
 
+import com.sheryians.major.controller.AdminController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MajorApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MajorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MajorApplication.class, args);
+    }
 }
